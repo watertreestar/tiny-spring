eb19ba603d7a7b7890c9d1107183533b7c183702
chenyaping <573002001@qq.com>
2020-09-14 14:30:06 +0800

    :test_tube: 添加测试代码

---
+++ src/test/java/com/xingyao/ioc/BeanFactoryTest.java
@@ -0,0 +1,28 @@
+package com.xingyao.ioc;
+
+import com.xingyao.ioc.factory.BeanFactory;
+import com.xingyao.ioc.xml.XmlBeanFactory;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @Author ranger
+ * @Date 2020/9/13 23:05
+ **/
+public class BeanFactoryTest {
+    String configLocation;
+    @Before
+    public void setUp(){
+        configLocation = this.getClass().getClassLoader().getResource("ioc.xml").getFile();
+    }
+
+
+    @Test
+    public void run(){
+        BeanFactory beanFactory = new XmlBeanFactory(configLocation);
+        Object car = beanFactory.getBean("car");
+        System.out.println(car);
+        Assert.assertNotNull(car);
+    }
+}
