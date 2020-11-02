e1b98bb93e200d1b19915555d43d6d8432d3d903
chenyaping <573002001@qq.com>
2020-11-02 20:36:38 +0800

    :art: start to complete proxy feature

---
+++ src/main/java/com/xingyao/aop/AdvisedSupport.java
@@ -0,0 +1,9 @@
+package com.xingyao.aop;
+
+/**
+ * @Author ranger
+ * @Date 2020/10/4 10:21
+ * 代理相关的元数据
+ **/
+public class AdvisedSupport {
+}
---
+++ src/main/java/com/xingyao/aop/AopProxy.java
@@ -0,0 +1,9 @@
+package com.xingyao.aop;
+
+/**
+ * @Author ranger
+ * @Date 2020/10/4 10:17
+ **/
+public interface AopProxy {
+    Object getProxy();
+}
